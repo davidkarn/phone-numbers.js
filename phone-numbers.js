4c0d940cedec238a88c7fd84c930401db7b27552
David Karn <davidkarnseattle@gmail.com>
2014-01-04 15:29:41 -0800

    Initial Commit

---
+++ phone-numbers.js
@@ -0,0 +1,11 @@
+var digits = [['0', 'zero', 'cero', 'sero'],
+	      ['1', 'one', 'won', 'wun', 'on', 'un'],
+	      ['2', 'two', 'too', 'to', 'tu', 'twoo'],
+	      ['3', 'three', 'tree', 'thrie', 'thre', 'tre'],
+	      ['4', 'four', 'for'],
+	      ['5', 'five', 'fiv'],
+	      ['6', 'six', 'zix', 'sicks', 'siks'],
+	      ['7', 'seven', 'sevn', 'sven'],
+	      ['8', 'eight', 'ate', 'aight'],
+	      ['9', 'nine', 'nin']];
+	      
