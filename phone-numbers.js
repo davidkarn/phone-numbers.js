7ac8e5bea0ee807398db24a6d1625871eaab8cc9
David Karn <davidkarnseattle@gmail.com>
2014-01-06 13:55:01 -0800

    wrote basic phone-number extrating function

--- phone-numbers.js
+++ phone-numbers.js
@@ -9,3 +9,19 @@ var digits = [['0', 'zero', 'cero', 'sero'],
 	      ['8', 'eight', 'ate', 'aight'],
 	      ['9', 'nine', 'nin']];
 	      
+function build_regex(match_digit) {
+    return new RegExp(
+	digits.reduce("\b(" + function(a, b) { return a.concat(b); }).join("|") + 
+	    (match_digit ? ")" 
+	     : "[^a-zA-Z0-9]*){10,10}\b"), "gim"); }
+
+function parse_number(str) {
+    var digs = str.match(build_regex(true)); 
+    return dig.map(function(d) {
+	for (var ds in digits) {
+	    if (digits[ds].indexOf(d.toLowerCase()) >= 0) {
+		return ds; }}}).join(""); }
+
+function extract_phone_numbers(str) {
+    var phone_numbers = str.match(build_regex());
+    return phone_numbers.map(parse_number); }
